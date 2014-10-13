320a4cb4aab52cab8d142db94bba993c308a17c4
Rémi Robert <th3o.smith@gmail.com>
2014-10-13 20:46:52 +0200

    Prise en charge des packets udp entrant sur le port 67 - Création du listener - Association d'un handler

--- dhcpUDP/connectionManager.go
+++ dhcpUDP/connectionManager.go
@@ -1,1 +1,49 @@
 package dhcpUDP
+
+import (
+	"fmt"
+	"net"
+)
+
+var listener *Listener
+
+type Listener struct {
+	C chan []byte
+}
+
+func InitListener(handler func([]byte)) {
+	listener = NewListener()
+	go listener.Run()
+
+	for {
+		tmp := <-listener.C
+		handler(tmp)
+	}
+}
+
+func NewListener() *Listener {
+	out := new(Listener)
+	out.C = make(chan []byte)
+	return out
+}
+
+func (l *Listener) Run() {
+
+	addr, err := net.ResolveUDPAddr("udp", ":67")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	sock, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	go func() {
+		for {
+			var buf [1024]byte
+			sock.ReadFromUDP(buf[:])
+			l.C <- buf[:]
+		}
+	}()
+}
